893d2cbe385be2c2d0837e6b14b734f5994d40c3
pkwiatkowski2000 <pawel.kwiatkowski2000@gmail.com>
2023-03-28 14:23:43 +0200

    28.03.2023

--- src/Zmienne_i__klasa_String.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class Zmienne_i_klasa_String {
-    @Test
-    public void examples() {
-
-    }
-}
---
+++ src/Zmienne_i_klasa_String.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+public class Zmienne_i_klasa_String {
+    @Test
+    public void examples() {
+        byte number;
+        number = 8;
+
+        int intNumber = 21;
+        long longNumber =123864L;
+
+        float floatNumber = 123.67F;
+        double doubleNumber = 8883.68;
+
+        boolean isTrue = false;
+
+        char letterA = 'a';
+        char alsoLetterA ='\u0061';
+        System.out.println(letterA);
+        System.out.println(alsoLetterA);
+
+        String text = "Moja ulubiona książka to \"Wszechświaty równoległe\"
+        System.out.println(text);
+    }
+}
