3588799d8b254556b5c902aabd4e2488a72dd9f2
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-04-03 15:00:08 +0200

    03.04.2023

--- src/PolaMetodyKlasyStatyczne/Employee.java
+++ src/PolaMetodyKlasyStatyczne/Employee.java
@@ -3,9 +3,20 @@ package PolaMetodyKlasyStatyczne;
 public class Employee {
     int id;
     static int nextId = 1;
+    int maxHolidayDays = 26;
+    int usedHolidayDays = 0;
 
     Employee() {
         id = nextId;
         nextId++;
+    }
+
+    void takeVacation(int days) {
+        if (usedHolidayDays + days <= maxHolidayDays) {
+            usedHolidayDays += days;
+            System.out.println("Przydzielono urlop w wysokości " + days + " dni.");
+        } else {
+            System.out.println("Urlop przekracza liczę dostępnych dni. Dostępne dni wolne: " + (maxHolidayDays - usedHolidayDays));
+        }
     }
 }
--- src/PolaMetodyKlasyStatyczne/Main.java
+++ src/PolaMetodyKlasyStatyczne/Main.java
@@ -3,14 +3,11 @@ package PolaMetodyKlasyStatyczne;
 public class Main {
     public static void main(String[] args) {
         Employee employee1 = new Employee();
-        Employee employee2 = new Employee();
-        Employee employee3 = new Employee();
 
-        System.out.println("Id pracownika pierwszego: " + employee1.id);
-        System.out.println("Id pracownika drugiego: " + employee2.id);
-        System.out.println("Id pracownika trzeciego: " + employee3.id);
-
-//        System.out.println("Następne id to: " + employee3.nextId);
-        System.out.println("Następne id to: " + Employee.nextId);
+        employee1.takeVacation(6);
+        employee1.takeVacation(10);
+        employee1.takeVacation(8);
+        employee1.takeVacation(2);
+        employee1.takeVacation(1);
     }
 }
